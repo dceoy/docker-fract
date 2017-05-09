88b6e642772dfff3763db26835fb5f80640a1174
dceoy <d.narsil@gmail.com>
2017-05-09 23:00:08 +0900

    Update deploy_do.sh

--- deploy_do.sh
+++ deploy_do.sh
@@ -9,8 +9,9 @@
 # Options:
 #   -h, --help        Print usage
 #   -v, --version     Print version information
-#   --droplet         Name of a Docker installed droplet [$FRACTUS_DROPLET]
-#   --fractus-yml     Path to fractus.yml [$FRACTUS_YML]
+#   -d, --droplet     Name of a Docker installed droplet [$FRACTUS_DROPLET]
+#   -f, --fractus-yml Path to fractus.yml [$FRACTUS_YML]
+#   -q, --quiet       Suppress output
 #   --build-only      Do not run a container after build
 
 set -e
@@ -22,7 +23,9 @@ COMMAND_VERSION='v0.1.0'
 COMMAND_PATH="$(dirname ${0})/$(basename ${0})"
 DROPLET="${FRACTUS_DROPLET}"
 FRACTUS_YML_PATH="$(eval echo ${FRACTUS_YML})"
-BUILD_ONLY=0
+Q_FLAG=''
+TO_NULL=''
+DC_CMD='up -d'
 
 function print_version {
   echo "${COMMAND_NAME}: ${COMMAND_VERSION}"
@@ -45,14 +48,17 @@ while [[ -n "${1}" ]]; do
     '-h' | '--help' )
       print_usage && exit 0
       ;;
-    '--droplet' )
+    '-d' | '--droplet' )
       DROPLET="${2}" && shift 2
       ;;
-    '--fractus-yml' )
+    '-f' | '--fractus-yml' )
       FRACTUS_YML_PATH="${2}" && shift 2
       ;;
+    '-q' | '--quiet' )
+      Q_FLAG='-q' && TO_NULL='> /dev/null 2>&1' && shift 1
+      ;;
     '--build-only' )
-      BUILD_ONLY=1 && shift 1
+      DC_CMD='build' && shift 1
       ;;
     * )
       abort "invalid argument \`${1}\`"
@@ -60,20 +66,18 @@ while [[ -n "${1}" ]]; do
   esac
 done
 
+set -u
+
 [[ -n "${DROPLET}" ]] || abort 'missing a droplet name'
 [[ -n "${FRACTUS_YML_PATH}" ]] || abort 'missing a path to fractus.yml'
 
-set -u
-
-tugboat ssh "${DROPLET}" \
-  -c 'wget https://raw.githubusercontent.com/dceoy/docker-fract/master/{Dockerfile,docker-compose.yml}'
-
-scp -i "$(tugboat config | awk '$1 == "ssh_key_path:" {print $2}')" \
+scp ${Q_FLAG} -i "$(tugboat config | awk '$1 == "ssh_key_path:" {print $2}')" \
   "${FRACTUS_YML_PATH}" \
   "root@$(tugboat info -a ip4 ${DROPLET} | tail -1):fractus.yml"
 
-tugboat ssh "${DROPLET}" \
-  -c 'apt -y update && apt -y upgrade && pip install -U pip docker-compose'
-
-tugboat ssh "${DROPLET}" \
-  -c "docker-compose $([[ ${BUILD_ONLY} -eq 0 ]] && echo 'up -d' || echo 'build')"
+tugboat ssh ${Q_FLAG} ${DROPLET} \
+  -c "apt -y update ${TO_NULL}; \
+      apt -y upgrade ${TO_NULL}; \
+      pip install -U ${Q_FLAG} pip docker-compose; \
+      wget ${Q_FLAG} https://raw.githubusercontent.com/dceoy/docker-fract/master/{Dockerfile,docker-compose.yml}; \
+      docker-compose ${DC_CMD} ${TO_NULL};"
